f25845560ee795dbc5d9f977d997edbbaa53d160
Sébastien RAILLET <sebastien.raillet@gmail.com>
2015-06-03 14:42:26 +0200

    Change the disconnection of the socket, send stop value when no hand is visible et start to write comments

--- src/C_LISTENER.cpp
+++ src/C_LISTENER.cpp
@@ -14,6 +14,9 @@
 #define CONSTRAINT_MIN_DIRECTION -50
 #define CONSTRAINT_MAX_DIRECTION 50
 
+// Difference between two positions that allow a sending on the socket
+#define DIFFERENCE 2
+
 C_LISTENER::C_LISTENER():
   Listener()
   ,m_socket()
@@ -48,18 +51,26 @@ void C_LISTENER::onConnect(const Controller &p_controller)
 }
 
 void C_LISTENER::onDisconnect(const Controller &p_controller)
-{
-  m_socket.Disconnect();
-}
-
-void C_LISTENER::onExit(const Controller &p_controller)
 {}
 
-void C_LISTENER::onFrame(const Controller &p_controller)
+void C_LISTENER::onExit(const Controller &p_controller)
+{
+  m_socket.Disconnect();
+  std::cout << "The socket is disconnected" << std::endl;
+}
+
+/**
+ * \fn void C_LISTENER::onFrame(const Controller &p_controller)
+ * \brief This function is called every time that a new frame is available
+ *
+ * \param p_controller The controller object that allow to get the current frame
+ * \return None
+ */
+void C_LISTENER::onFrame(const Controller& p_controller)
 {
   float l_thumb_y_position, l_pinky_y_position, l_difference_thumb_pinky, l_hand_position = 0;
   int l_speed, l_direction = 0;
-  const Frame frame = p_controller.frame();
+  const Frame frame = p_controller.frame(); // Get the frame
 
   /*std::cout << "Frame id: " << frame.id()
     << ", timestamp: " << frame.timestamp()
@@ -68,49 +79,49 @@ void C_LISTENER::onFrame(const Controller &p_controller)
     << ", tools: " << frame.tools().count()
     << ", gestures: " << frame.gestures().count() << std::endl;*/
 
-  HandList hands = frame.hands();
-  for (HandList::const_iterator hl = hands.begin(); hl != hands.end(); ++hl) 
+  HandList hands = frame.hands(); // Get the hands list
+  if ( hands.isEmpty() )
   {
-    // Get the first hand
+    m_socket.Send(CreateMessage(50, 50));
+  }
+  for (HandList::const_iterator hl = hands.begin(); hl != hands.end(); ++hl) // Read though the hand list 
+  {
     const Hand hand = *hl;
-    l_hand_position = hand.palmPosition().z;
+    l_hand_position = hand.palmPosition().z; // Recover the hand position that reprents the speed
     //std::cout << "Hand position z axis : " << hand.palmPosition().z << std::endl;
 
-    // Get fingers
-    const FingerList fingers = hand.fingers();
-    for (FingerList::const_iterator fl = fingers.begin(); fl != fingers.end(); ++fl) 
+    const FingerList fingers = hand.fingers(); // Get the fingers list
+    for (FingerList::const_iterator fl = fingers.begin(); fl != fingers.end(); ++fl) // Read though the finger list
     {
       const Finger finger = *fl;
-      if ( Finger::TYPE_THUMB == finger.type() )
+      if ( Finger::TYPE_THUMB == finger.type() ) // Verify if the finger is the thumb
       {
         Bone::Type boneType = static_cast<Bone::Type>(Bone::TYPE_DISTAL);
-        Bone bone = finger.bone(boneType);
-        l_thumb_y_position = bone.nextJoint().y;
+        Bone bone = finger.bone(boneType); // Get the distal bone that correspond at this finger
+        l_thumb_y_position = bone.nextJoint().y; // Get the y position of the end of the distal bone 
         //std::cout << "Thumb distal bone y position : " << l_thumb_y_position << std::endl;
       }
-      else if ( Finger::TYPE_PINKY == finger.type() )
+      else if ( Finger::TYPE_PINKY == finger.type() ) // Verify if the finger is the pinky
       {
         Bone::Type boneType = static_cast<Bone::Type>(Bone::TYPE_DISTAL);
-        Bone bone = finger.bone(boneType);
-        l_pinky_y_position = bone.nextJoint().y;
+        Bone bone = finger.bone(boneType); // Get the distal bone that correspond at this finger
+        l_pinky_y_position = bone.nextJoint().y; // Get the y position of the end of the distal bone
         //std::cout << "Pinky distal bone y position : " << l_pinky_y_position << std::endl;
       }
     }
 
-    if( hand.isRight() ) // Traitement main droite
+    if( hand.isRight() ) // Verify if is the right hand 
     {         
       l_difference_thumb_pinky = l_thumb_y_position - l_pinky_y_position;
-      std::cout << "Right hand" << std::endl;
     }
-    else // Traitement main gauche
+    else // is the left hand  
     { 
       l_difference_thumb_pinky = l_pinky_y_position - l_thumb_y_position;
-      std::cout << "Left hand" << std::endl;
     }
   }
 
   MapAndConstraint(l_hand_position, l_difference_thumb_pinky, l_speed, l_direction);
-  if ( (l_direction >= m_previous_direction+2) || (l_direction <= m_previous_direction-2) )
+  if ( (l_direction >= m_previous_direction+DIFFERENCE) || (l_direction <= m_previous_direction-DIFFERENCE) )
   {
     m_previous_direction = l_direction;
     m_socket.Send(CreateMessage(l_speed, l_direction));
@@ -133,6 +144,16 @@ void C_LISTENER::onServiceConnect(const Controller &p_controller)
 void C_LISTENER::onServiceDisconnect(const Controller &p_controller)
 {}
 
+/**
+ * \fn void C_LISTENER::MapAndConstraint(float p_speed_in, float p_direction_in, int& p_speed_out, int& p_direction_out)
+ * \brief This function map the speed and direction and constraint the values
+ *
+ * \param p_speed_in The speed value that is necessary to map and constraint
+ * \param p_direction_in The direction value that is necessary to map and constraint
+ * \param p_speed_out The speed value mapped and constrainted
+ * \param p_direction_out The direction value mapped and constrainted
+ * \return None
+ */
 void C_LISTENER::MapAndConstraint(float p_speed_in, float p_direction_in, int& p_speed_out, int& p_direction_out)
 {
   // Constraint and map the speed and direction value
